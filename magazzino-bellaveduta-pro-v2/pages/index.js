db9d997a91acbf9f757fb5219d4b536182d89efb
rich919 <michelericco95@gmail.com>
2025-06-20 20:39:53 +0200

    Update index.js

--- magazzino-bellaveduta-pro-v2/pages/index.js
+++ magazzino-bellaveduta-pro-v2/pages/index.js
@@ -1,15 +1,10 @@
-export default function LoginPage() {
+export default function HomePage() {
   return (
     <div style={{ padding: "2rem", fontFamily: "Arial" }}>
-      <h1>Login</h1>
-      <form style={{ display: "flex", flexDirection: "column", maxWidth: "300px" }}>
-        <label>Username:</label>
-        <input type="text" placeholder="Inserisci username" />
-        <label>Password:</label>
-        <input type="password" placeholder="Inserisci password" />
-        <button style={{ marginTop: "1rem" }}>Accedi</button>
-      </form>
+      <h1>Benvenuto al Magazzino Bellaveduta</h1>
+      <p>Seleziona una sezione dal menu per iniziare.</p>
     </div>
   );
 }
+
 // Home page login
